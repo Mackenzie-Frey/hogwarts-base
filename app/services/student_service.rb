91b8f450e9738001c535385541c17b26180197ea
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-03 15:08:46 -0600

    Refactor in progress. Next error to overcome is: conn.get("house/#{@house}") ArgumentError: wrong number of arguments (given 0, expected 1) from /Users/mackenziefrey/turing/3module_B/class/hogwarts-base/app/services/student_service.rb:6:in `conn'

--- app/services/student_service.rb
+++ app/services/student_service.rb
@@ -3,9 +3,19 @@ class StudentService
     @house = params['house']
   end
 
+  # def conn(url)
+  #   response = Faraday.get(:url => 'http://hogwarts-it.herokuapp.com/api/v1/') do |faraday|
+  #     faraday.params['api_key'] = "#{ENV['hogwarts_api_key']}"
+  #     faraday.adapter  Faraday.default_adapter
+  #   end
+  #   JSON.parse(response.body)
+  # end
+
   def house_students
+    # binding.pry
+    # conn.get("house/#{@house}")
+    # binding.pry
     response = Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/#{@house}?api_key=#{ENV['hogwarts_api_key']}")
     result = JSON.parse(response.body)
-    # binding.pry
   end
 end
