da485a672a369d68dc9ebd00742482ccf55d3f3b
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-03 14:15:14 -0600

    Adds to index method with Faraday request.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+binding.pry
+    @facade = Faraday.get('http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=3fhZMZNXVndxk51mILsdVnGN6tF8KBI6')
+
   end
 end
