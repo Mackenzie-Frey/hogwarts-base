e6356aa71a23ac8374340b01c185039d98ae73a3
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-03 14:39:58 -0600

    Passes params into a Service.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,10 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get('http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=3fhZMZNXVndxk51mILsdVnGN6tF8KBI6')
-    result = JSON.parse(response.body)
+
+    # response = Faraday.get('http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=3fhZMZNXVndxk51mILsdVnGN6tF8KBI6')
+    # result = JSON.parse(response.body)
+
+    result = StudentService.new(params['house'])
     @facade = Facade.new(result).students
   end
 end
