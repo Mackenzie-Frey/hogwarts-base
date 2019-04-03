442f88cc3acf3111037dc83243e38429321cc0de
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-03 14:09:50 -0600

    Adds view and controller.

--- spec/features/user_can_see_students_in_each_house_spec.rb
+++ spec/features/user_can_see_students_in_each_house_spec.rb
@@ -2,12 +2,9 @@ require 'rails_helper'
 
 context 'As a user visiting / ' do
   it 'I can see access the different students in each house' do
-    # user = create(:user)
-    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit '/'
 
-    #from drop down
     select "Slytherin", from: "house"
 
     click_on "Get Students"
