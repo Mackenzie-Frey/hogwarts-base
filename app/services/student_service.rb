33bce1a8c37edf207cfb11b6b8d8f798021adac9
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-04 20:03:26 -0600

    Functioning API request.

--- app/services/student_service.rb
+++ app/services/student_service.rb
@@ -4,23 +4,29 @@ class StudentService
   end
 
   def house_students
-    get_json("house/#{@house}")
+    JSON.parse((conn.get).body)
+    house_id
   end
 
-  def get_json(url)
-    response = conn.get(url)
-    JSON.parse(response.body)
+  def house_id
+    binding.pry
+    id = nil
+    conn.each do |house|
+      if house['name'] == @house
+        id = house['id']
+      end
+    end
+    binding.pry
+    id
   end
+
+  # curl -H 'X_API_KEY: oPGaRUMm7Cpisj6fosH8VhIgjnxx8zh1' 'https://hogwarts-as-a-service.herokuapp.com/api/v1/house'
+
 
   def conn
-    Faraday.new(url: 'http://hogwarts-it.herokuapp.com/api/v1/') do |faraday|
-      faraday.params['api_key'] = ENV['hogwarts_api_key']
+    Faraday.new(url: 'http://hogwarts-as-a-service.herokuapp.com/api/v1/house') do |faraday|
+      faraday.headers['x_api_key'] = ENV['hogwarts_as_a_service_api_key']
       faraday.adapter  Faraday.default_adapter
     end
   end
 end
-
-# https://hogwarts-as-a-service.herokuapp.com/api/v1/house/
-# x_api_key
-# response = Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/#{@house}?api_key=#{ENV['hogwarts_api_key']}")
-# hogwarts_as_a_service_api_key
